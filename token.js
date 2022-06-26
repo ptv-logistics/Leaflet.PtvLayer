60e3404b0bef99560cb0a65c0f65520bc7784543
Oliver Heilig <oliver.heilig@yahoo.de>
2022-06-26 10:45:36 +0200

    Update token.js

--- token.js
+++ token.js
@@ -1,3 +1,2 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2022-04-30T06:02:35Z and was auto-generated @ 2022-02-07T22:39:09Z.
-var token = 'F49736E2-440D-4918-8B47-0C475F5A2B28';
+var token = 'F52DD135-A3D7-4CCD-A9AE-B2E3FF2A52F2';
